545569aac4a5264c1aafc45dfd2b0385f187caef
Eduardo Sanchez <a20122509@pucp.pe>
2021-12-09 18:32:51 -0500

    avance3vista2

--- app/src/main/java/com/example/encuentralo/MainActivity2.kt
+++ app/src/main/java/com/example/encuentralo/MainActivity2.kt
@@ -2,16 +2,17 @@ package com.example.encuentralo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.encuentralo.databinding.ActivityMainBinding
+import com.example.encuentralo.databinding.ActivityMain2Binding
+
 
 class MainActivity2 : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMain2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.imageButton3.setOnClickListener {
-            val myIntent = Intent(this, ThirdActivity::class.java)
+            val myIntent = Intent(this, ActivityMain2Binding::class.java)
             startActivity(myIntent)
         }
     }
